de80bd336e1031735fc8b627ff67bd8dc5b02ec4
Akash Gupta <akashg.official18@gmail.com>
2023-05-06 19:10:00 +0530

    third commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
+
   render() {
+    let {title, description} = this.props
     return (
       <div>
-        This is a news item
+        <div className="card" style={{width: "18rem;"}}>
+          <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/E0EC/production/_129508575_ksb.png" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="/newsdetail" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
